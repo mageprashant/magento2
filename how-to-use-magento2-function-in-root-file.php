2be931a3511ddc057ba682ea2e1596f411e9f6d9
Prashant Patel <39878810+mageprashant@users.noreply.github.com>
2018-09-28 15:57:15 +0530

    How to use magento2 function in root file
    How to use magento2 function in root file

---
+++ how-to-use-magento2-function-in-root-file.php
@@ -0,0 +1,17 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+use Magento\Framework\App\Bootstrap;
+require __DIR__ . '/app/bootstrap.php';
+
+$bootstrap = Bootstrap::create(BP, $_SERVER);
+$objectManager = $bootstrap->getObjectManager();
+$state = $objectManager->get('\Magento\Framework\App\State');
+$state->setAreaCode('admin'); //frontend
+
+EX :-
+$feed = $objectManager->get('FILE PATH');
+$feed->METHOD NAME();
